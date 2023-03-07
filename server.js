c437c8329e19efa3b0b132e5e60c30744568211d
imaaref <127018593+imaaref@users.noreply.github.com>
2023-03-07 15:03:05 +0400

    updated code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
-const url = "https://" + process.env.PROJECT_DOMAIN + ".glitch.me";
+const username =  "admin";
+const password = "password";
+const url = "https://" + process.env.PROJECT_DOMAIN + ".cyclic.app";
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
